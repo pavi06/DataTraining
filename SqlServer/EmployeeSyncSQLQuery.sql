f1101acfdddba4a7c86bc17c88465aff578c48e0
pavi06 <pavi06saishivu@gmail.com>
2024-08-25 10:19:20 +0530

    sql file edited

--- SqlServer/EmployeeSyncSQLQuery.sql
+++ SqlServer/EmployeeSyncSQLQuery.sql
@@ -14,87 +14,67 @@ isActive bit
 );
 go
 
+exec sp_rename 'employee_trgt.isActive', 'isDeleted', 'COLUMN';
+exec sp_rename 'employee_trgt.UpdateTimeFlag', 'UpdatedTime', 'COLUMN';
+exec sp_rename 'employee_src.UpdateTimeFlag', 'UpdatedTime', 'COLUMN';
 
 select * from employee_src
 select * from employee_trgt
 go
 
-create trigger trg_UpdateEmployeeSrc
+create or alter trigger trg_UpdateEmployeeSrc
 on employee_src
 after update
 as
 begin
     update src
-    set UpdateTimeFlag = GetDate()
+    set UpdatedTime = GetDate()
     from employee_src src
     inner join inserted i on src.EmpId = i.EmpId;
 end;
 go
 
-/*
-create procedure InsertRecord
+create or alter procedure SyncTable(@debugFlag int=0)
 as
 begin
-    insert into employee_trgt (EmpId, EmpName, Salary, UpdateTimeFlag, isActive)
-    select src.EmpId, src.EmpName, src.Salary, src.UpdateTimeFlag, 1
-    from employee_src src
-    left join employee_trgt trgt on src.EmpId = trgt.EmpId
-    where trgt.EmpId is null;
+	 if @debugFlag=1
+		 select trgt.EmpId, trgt.EmpName, trgt.Salary, trgt.UpdatedTime, trgt.isDeleted from employee_trgt trgt
+		 inner join(
+			select EmpId, max(UpdatedTime) as MaxUpdatedTime from employee_trgt 
+			group by EmpId
+		 ) latest_trgt on trgt.EmpId = latest_trgt.EmpId and trgt.UpdatedTime = latest_trgt.MaxUpdatedTime order by EmpId;
+
+	 insert into employee_trgt (EmpId, EmpName, Salary, UpdatedTime, isDeleted)
+	 select src.EmpId, src.EmpName, src.Salary, src.UpdatedTime, 0
+	 from employee_src src
+	 left join (
+        select EmpId, max(UpdatedTime) AS MaxUpdatedTime from employee_trgt group by EmpId
+     )latest_trgt on src.EmpId = latest_trgt.EmpId
+	 where latest_trgt.EmpId is null;
+
+	 insert into employee_trgt (EmpId, EmpName, Salary, UpdatedTime, isDeleted)
+	 select src.EmpId, src.EmpName, src.Salary, src.UpdatedTime, 0 from employee_src src
+	 left join(
+        select EmpId, max(UpdatedTime) as MaxUpdatedTime from employee_trgt group by EmpId
+     )latest_trgt on src.EmpId = latest_trgt.EmpId
+     where src.UpdatedTime <> latest_trgt.MaxUpdatedTime
+
+	 update trgt set isDeleted = 1 from employee_trgt trgt
+	 left join employee_src src on trgt.EmpId = src.EmpId
+     where src.EmpId is null and trgt.isDeleted = 0;
 end;
 go
-*/
+
+exec SyncTable;
+exec SyncTable @debugFlag = 1;
+
+insert into employee_src(EmpName,Salary) values('Roke', 90000);
+update employee_src set salary = 80000 where EmpId=14;
+delete from employee_src where EmpId=14;
 
 
-/*
-create or alter procedure UpdateRecord
-as
-begin
-	with RankedEmployeeData as (
-	select EmpId, EmpName, Salary, UpdateTimeFlag, isActive,
-    Rank() Over (Partition By EmpId Order By UpdateTimeFlag desc) as ru
-	From employee_trgt
-	)
 
-	insert into employee_trgt (EmpId, EmpName, Salary, UpdateTimeFlag, isActive)
-    select src.EmpId, src.EmpName, src.Salary, src.UpdateTimeFlag, 1
-    from employee_src src
-	left join RankedEmployeeData rd on src.EmpId = rd.EmpId and rd.ru=1 
-    where src.UpdateTimeFlag <> rd.UpdateTimeFlag;
-end;
-go
-
-create procedure SoftDelete
-as
-begin
-    update trgt
-    set isActive = 0
-    from employee_trgt trgt
-	left join employee_src src on trgt.EmpId = src.EmpId
-    where src.EmpId is null and src.isActive = 1;
-end;
-go
-*/
-
-/*
-create or alter procedure SyncTable(@defaultFlag int=0)
-as
-begin
-	 if @defaultFlag=1
-	 begin
-		with RankedEmployeeData as (
-		select EmpId, EmpName, Salary, UpdateTimeFlag, isActive,
-		Rank() Over (Partition By EmpId Order By UpdateTimeFlag desc) as ru
-		From employee_trgt
-		)select EmpId,EmpName,Salary,UpdateTimeFlag,isActive 
-		from RankedEmployeeData where ru = 1;
-	 end;
-	 exec InsertRecord
-	 exec UpdateRecord
-	 exec SoftDelete
-end;
-go*/
-
-create or alter procedure SyncTable(@defaultFlag int = 0)
+/*create or alter procedure SyncTable(@defaultFlag int = 0)
 as
 begin
     if @defaultFlag = 1
@@ -136,15 +116,8 @@ begin
 		update set target.isActive = 0;
 end;
 go
+*/
 
-exec SyncTable;
-exec SyncTable @defaultFlag = 1;
 
-insert into employee_src(EmpName,Salary) values('Viji', 90000);
-update employee_src set salary = 9000 where EmpId=7;
-delete from employee_src where EmpId=3;
-
-select * from employee_src
-select * from employee_trgt
 
  
